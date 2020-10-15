9b27aa0290196a19c94ca362404a6b4e1803d6b3
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-14 19:39:24 -0700

    Update tensorflow_alexnet.py

--- alexnet_cifar/tensorflow_alexnet.py
+++ alexnet_cifar/tensorflow_alexnet.py
@@ -11,14 +11,14 @@ class TensorFlow_AlexNet:
         # from: https://towardsdatascience.com/implementing-alexnet-cnn-architecture-using-tensorflow-2-0-and-keras-2113e090ad98
         # updated to match existing pytorch model
         self.model = keras.models.Sequential([
-            keras.layers.Conv2D(filters=64, kernel_size=(11,11), strides=4, activation='relu', input_shape=(32,32,3)),
-            keras.layers.MaxPool2D(pool_size=(3,3), strides=(2, 2)),
+            keras.layers.Conv2D(filters=64, kernel_size=(11,11), strides=4, activation='relu', input_shape=(32, 32, 3)),
+            keras.layers.MaxPool2D(pool_size=(3,3), strides=(2, 2), padding="same"),
             keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=1, activation='relu', padding="same"),
-            keras.layers.MaxPool2D(pool_size=(3,3), strides=(2, 2)),
+            keras.layers.MaxPool2D(pool_size=(3,3), strides=(2, 2), padding="same"),
             keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=1, activation='relu', padding="same"),
             keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=1, activation='relu', padding="same"),
             keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=1, activation='relu', padding="same"),
-            keras.layers.MaxPool2D(pool_size=(3,3), strides=(2, 2)),
+            keras.layers.MaxPool2D(pool_size=(3,3), strides=(2, 2), padding="same"),
             keras.layers.Flatten(),
             keras.layers.Dense(4096, activation='relu'),
             keras.layers.Dropout(config['dropout']),
