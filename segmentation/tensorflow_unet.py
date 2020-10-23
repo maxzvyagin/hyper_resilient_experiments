d335e21cd8a6d85250f07cba6cf0a6a0d0a1d919
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-23 14:18:43 -0700

    Update tensorflow_unet.py

--- segmentation/tensorflow_unet.py
+++ segmentation/tensorflow_unet.py
@@ -10,7 +10,8 @@ def cityscapes_tf_objective(config):
     model.compile(optimizer=opt,loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
     # fit model on cityscapes data
-    (x_train, y_train), (x_test, y_test) = keras.datasets.cityscapes.load_data()
+    (x_train, y_train), (x_test, y_test) = tfds.load('cityscapes',
+                                                     split=['train', 'test'], shuffle_files=False, as_supervised=True)
     res = model.fit(x_train, y_train, epochs=config['epochs'], batch_size=config['batch_size'])
     res_test = model.evaluate(x_test, y_test)
     return (res_test[1], model)
