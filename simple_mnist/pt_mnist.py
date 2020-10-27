04bda5bf680692e1c93b3df1b46e0ec881d0020c
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-26 19:58:34 -0700

    Update pt_mnist.py

--- simple_mnist/pt_mnist.py
+++ simple_mnist/pt_mnist.py
@@ -78,5 +78,5 @@ def mnist_pt_objective(config):
     return (model.test_accuracy, model.model)
 
 if __name__ == "__main__":
-    test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1}
+    test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1, 'dropout':0.5}
     res = mnist_pt_objective(test_config)
