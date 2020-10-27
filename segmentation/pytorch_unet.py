7718a7c6c12e2c34748f3b21e9439e3ed08593e8
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-26 17:08:27 -0700

    Update pytorch_unet.py

--- segmentation/pytorch_unet.py
+++ segmentation/pytorch_unet.py
@@ -85,6 +85,6 @@ def cityscapes_pt_objective(config):
 ### two different objective functions, one for cityscapes and one for GIS
 
 if __name__ == "__main__":
-    torch.backends.cudnn.enabled = False
+    #torch.backends.cudnn.enabled = False
     test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1}
     res = cityscapes_pt_objective(test_config)
