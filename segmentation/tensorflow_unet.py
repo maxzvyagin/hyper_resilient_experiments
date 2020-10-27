0d63c70159f583fc6bc1ff53b93cafe650d7e565
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-27 10:57:15 -0700

    Update tensorflow_unet.py

--- segmentation/tensorflow_unet.py
+++ segmentation/tensorflow_unet.py
@@ -41,5 +41,5 @@ def get_cityscapes():
 
 if __name__ == "__main__":
     with tf.device('/device:GPU:7'):
-        test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1}
+        test_config = {'batch_size': 1, 'learning_rate': .001, 'epochs': 1}
         res = cityscapes_tf_objective(test_config)
