baa9c3889b86ff94e8838170a159a67c739f128f
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-28 12:16:52 -0700

    Update pytorch_unet.py

--- segmentation/pytorch_unet.py
+++ segmentation/pytorch_unet.py
@@ -115,5 +115,5 @@ def cityscapes_pt_objective(config):
 ### two different objective functions, one for cityscapes and one for GIS
 
 if __name__ == "__main__":
-    test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1}
+    test_config = {'batch_size': 32, 'learning_rate': .001, 'epochs': 1}
     res = cityscapes_pt_objective(test_config)
