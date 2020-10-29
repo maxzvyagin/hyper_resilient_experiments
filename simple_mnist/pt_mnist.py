6cedce6cd28360c323df8c1b8c8df751fbedc4bf
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-29 14:05:39 -0700

    cuda debug

--- simple_mnist/pt_mnist.py
+++ simple_mnist/pt_mnist.py
@@ -3,6 +3,7 @@ import pytorch_lightning as pl
 import torchvision
 import torch
 import statistics
+import os
 
 
 class NumberNet(pl.LightningModule):
@@ -72,6 +73,7 @@ class NumberNet(pl.LightningModule):
 
 
 def mnist_pt_objective(config):
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
     model = NumberNet(config)
     #trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[0, 1, 2, 3], distributed_backend='ddp')
     trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[1, 2, 3, 4], distributed_backend='ddp')
