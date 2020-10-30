cb90d3b00fe79ccfba47a7bc7eb62158f2e54032
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-30 13:10:23 -0700

    Update pt_mnist.py

--- simple_mnist/pt_mnist.py
+++ simple_mnist/pt_mnist.py
@@ -44,27 +44,14 @@ class NumberNet(pl.LightningModule):
 
     def training_step(self, train_batch, batch_idx):
         x, y = train_batch
-        print(len(x))
         return {'forward': self.forward(x), 'expected': y}
-        #return self.forward(x)
 
 
     def training_step_end(self, outputs):
         # only use when  on dp
-        print(outputs)
-        yhat = []
-        y = []
-        for res in outputs:
-            yhat.extend(res['forward'])
-            y.extend(res['expected'])
-        print(yhat)
-        print(y)
-        yhat = torch.Tensor(yhat)
-        y = torch.Tensor(y)
-        #outputs = torch.cat(outputs, dim=1)
-        loss = self.criterion(yhat, y)
+        loss = self.criterion(outputs['forward'], outputs['expected'])
         logs = {'train_loss': loss}
-        return {'loss': loss}
+        return {'loss': loss, 'logs': logs}
 
     def test_step(self, test_batch, batch_idx):
         x, y = test_batch
@@ -102,5 +89,5 @@ def mnist_pt_objective(config):
 
 
 if __name__ == "__main__":
-    test_config = {'batch_size': 5, 'learning_rate': .001, 'epochs': 1, 'dropout': 0.5}
+    test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1, 'dropout': 0.5}
     res = mnist_pt_objective(test_config)
