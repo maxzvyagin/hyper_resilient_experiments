0fca4817abc18d246ef20a334ada9e018d324fbe
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-30 10:16:48 -0700

    debugging multi gpu runs

--- simple_mnist/pt_mnist.py
+++ simple_mnist/pt_mnist.py
@@ -75,8 +75,8 @@ class NumberNet(pl.LightningModule):
 def mnist_pt_objective(config):
     os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
     model = NumberNet(config)
-    #trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[0, 1, 2, 3], distributed_backend='ddp')
-    trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[0], distributed_backend='ddp')
+    trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[0, 1, 2, 3], distributed_backend='dp')
+    #trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[0], distributed_backend='ddp')
     #trainer = pl.Trainer(max_epochs=config['epochs'], gpus=4, auto_select_gpus=True)
     #trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[8, 9, 10, 11], distributed_backend='ddp')
     trainer.fit(model)
