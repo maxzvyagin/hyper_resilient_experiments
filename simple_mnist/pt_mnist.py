75e5206988904360da5ae6e2db472723406c616f
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-10-30 09:32:25 -0700

    Update pt_mnist.py

--- simple_mnist/pt_mnist.py
+++ simple_mnist/pt_mnist.py
@@ -75,7 +75,7 @@ class NumberNet(pl.LightningModule):
 def mnist_pt_objective(config):
     os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
     model = NumberNet(config)
-    trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[0, 1, 2, 3], distributed_backend='ddp', num_nodes=1)
+    trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[0, 1, 2, 3], distributed_backend='ddp', num_nodes=1, num_processes=1)
     #trainer = pl.Trainer(max_epochs=config['epochs'], gpus=4, auto_select_gpus=True)
     #trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[8, 9, 10, 11], distributed_backend='ddp')
     trainer.fit(model)
