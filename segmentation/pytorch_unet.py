242244a8dc81bbf1e28a1ac087291c8bc6af87c6
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-03 18:38:43 -0800

    cityscapes testing

--- segmentation/pytorch_unet.py
+++ segmentation/pytorch_unet.py
@@ -135,5 +135,5 @@ def gis_pt_pbjective(config):
 if __name__ == "__main__":
     # Note that batch size is per gpu
     test_config = {'batch_size': 1, 'learning_rate': .001, 'epochs': 1}
-    #res = segmentation_pt_objective(test_config)
-    res = segmentation_pt_objective(test_config, dataset="gis")
+    res = segmentation_pt_objective(test_config)
+    #res = segmentation_pt_objective(test_config, dataset="gis")
