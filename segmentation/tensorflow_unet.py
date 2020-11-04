165fc2d10d32f02f3a0090c6f639b5096e1ce19a
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-04 10:39:31 -0800

    Update tensorflow_unet.py

--- segmentation/tensorflow_unet.py
+++ segmentation/tensorflow_unet.py
@@ -8,7 +8,7 @@ sys.path.append("/home/mzvyagin/hyper_resilient/segmentation")
 from gis_preprocess import tf_gis_test_train_split
 
 def cityscapes_tf_objective(config, classes=30):
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '4,5,6,7'
     #os.environ['CUDA_VISIBLE_DEVICES'] = '4'
     gpus = tf.config.experimental.list_physical_devices('GPU')
     tf.config.experimental.set_visible_devices(gpus[4:8], 'GPU')
@@ -29,8 +29,10 @@ def cityscapes_tf_objective(config, classes=30):
 
 # same model just using gis data instead
 def gis_tf_objective(config, classes=1):
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '4,5,6,7'
     #os.environ['CUDA_VISIBLE_DEVICES'] = '4'
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_visible_devices(gpus[4:8], 'GPU')
     strategy = tf.distribute.MirroredStrategy(devices=["/gpu:4", "/gpu:5", "/gpu:6", "/gpu:7"])
     with strategy.scope():
         model = tf.keras.Sequential()
