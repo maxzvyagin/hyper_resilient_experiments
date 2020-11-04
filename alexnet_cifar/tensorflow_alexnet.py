c1c95feabd79d8fb26ebb0b1198cf7b361a690c0
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-04 15:21:12 -0800

    Update tensorflow_alexnet.py
    fix tf gpu config

--- alexnet_cifar/tensorflow_alexnet.py
+++ alexnet_cifar/tensorflow_alexnet.py
@@ -16,8 +16,8 @@ class TensorFlow_AlexNet:
         # define the model using alexnet architecture
         # from: https://towardsdatascience.com/implementing-alexnet-cnn-architecture-using-tensorflow-2-0-and-keras-2113e090ad98
         # updated to match existing pytorch model
-        gpus = tf.config.experimental.list_physical_devices('GPU')
-        tf.config.experimental.set_visible_devices(gpus[4:8], 'GPU')
+        # gpus = tf.config.experimental.list_physical_devices('GPU')
+        # tf.config.experimental.set_visible_devices(gpus[4:8], 'GPU')
         strategy = tf.distribute.MirroredStrategy(devices=["/gpu:4", "/gpu:5", "/gpu:6", "/gpu:7"])
         #strategy = tf.distribute.MirroredStrategy(devices=["/gpu:4"])
         with strategy.scope():
