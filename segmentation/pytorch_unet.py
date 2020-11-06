06141812d127582b85afd829822e2c82411dafc4
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-05 20:49:10 -0800

    Update pytorch_unet.py

--- segmentation/pytorch_unet.py
+++ segmentation/pytorch_unet.py
@@ -150,14 +150,14 @@ def gis_pt_pbjective(config):
 ### two different objective functions, one for cityscapes and one for GIS
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--batch')
-    args = parser.parse_args()
-    if args.batch:
-        batch_size = args.batch
-    else:
-        batch_size = 4
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-b', '--batch')
+    # args = parser.parse_args()
+    # if args.batch:
+    #     batch_size = args.batch
+    # else:
+    #     batch_size = 4
     # Note that batch size is per gpu
-    test_config = {'batch_size': batch_size, 'learning_rate': .001, 'epochs': 1}
+    test_config = {'batch_size': 16, 'learning_rate': .001, 'epochs': 1}
     res = segmentation_pt_objective(test_config)
     # res = segmentation_pt_objective(test_config, dataset="gis")
