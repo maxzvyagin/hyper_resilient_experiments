8e1f0dbb10267e0390cf109cb45b1d34a5a9599e
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-06 09:14:21 -0800

    Update bi_tune.py

--- bi_tune.py
+++ bi_tune.py
@@ -204,7 +204,7 @@ if __name__ == "__main__":
         hyperparameters = [(0.00001, 0.1),  # learning_rate
                            (0.2, 0.9),  # dropout
                            (10, 100),  # epochs
-                           (10, 100)]  # batch size
+                           (10, 250)]  # batch size
     else:
         hyperparameters = [(0.00001, 0.1),  # learning_rate
                            (0.2, 0.9),  # dropout
