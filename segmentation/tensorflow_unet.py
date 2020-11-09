80e191181a5b3436b05587675ffac06b63f3b95b
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-09 15:00:24 -0800

    fixing segmentation code

--- segmentation/tensorflow_unet.py
+++ segmentation/tensorflow_unet.py
@@ -11,9 +11,10 @@ from gis_preprocess import tf_gis_test_train_split
 
 def cityscapes_tf_objective(config, classes=30):
     tf.random.set_seed(0)
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    tf.config.experimental.set_visible_devices(gpus[4:8], 'GPU')
-    strategy = tf.distribute.MirroredStrategy(devices=["/gpu:4", "/gpu:5", "/gpu:6", "/gpu:7"])
+    # gpus = tf.config.experimental.list_physical_devices('GPU')
+    # tf.config.experimental.set_visible_devices(gpus[4:8], 'GPU')
+    strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1", "/gpu:2", "/gpu:3", "/gpu:4", "/gpu:5",
+                                                       "/gpu:6", "/gpu:7"])
     with strategy.scope():
         model = tf.keras.Sequential()
         model.add(sm.Unet('resnet34', encoder_weights=None, classes=classes, activation=None))
@@ -31,9 +32,10 @@ def cityscapes_tf_objective(config, classes=30):
 # same model just using gis data instead
 def gis_tf_objective(config, classes=1):
     tf.random.set_seed(0)
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    tf.config.experimental.set_visible_devices(gpus[4:8], 'GPU')
-    strategy = tf.distribute.MirroredStrategy(devices=["/gpu:4", "/gpu:5", "/gpu:6", "/gpu:7"])
+    # gpus = tf.config.experimental.list_physical_devices('GPU')
+    # tf.config.experimental.set_visible_devices(gpus[4:8], 'GPU')
+    strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1", "/gpu:2", "/gpu:3", "/gpu:4", "/gpu:5",
+                                                       "/gpu:6", "/gpu:7"])
     with strategy.scope():
         model = sm.Unet('resnet34', encoder_weights=None, classes=classes, input_shape=(None, None, 4),
                         activation="sigmoid")
