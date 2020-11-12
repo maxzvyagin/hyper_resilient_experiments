a42e30d3be3c20135ea5e856ab8fb95f2c42cd7c
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-11 20:26:35 -0800

    fixing unet

--- segmentation/tensorflow_unet.py
+++ segmentation/tensorflow_unet.py
@@ -9,6 +9,8 @@ from tensorflow import keras
 sys.path.append("/home/mzvyagin/hyper_resilient/segmentation")
 from gis_preprocess import tf_gis_test_train_split
 
+from UNet.tensorflow_unet import TensorFlow_UNet
+
 # import os
 
 
@@ -23,7 +25,7 @@ def cityscapes_tf_objective(config, classes=30):
     #                                                    "/gpu:6", "/gpu:7"])
     # with strategy.scope():
     model = tf.keras.Sequential()
-    model.add(sm.Unet('resnet34', encoder_weights=None, classes=classes, activation=None))
+    model.add(TensorFlow_UNet(3, 30))
     model.add(tf.keras.layers.Dense(30, activation=tf.nn.log_softmax))
     opt = tf.keras.optimizers.Adam(learning_rate=config['learning_rate'])
     model.compile(optimizer=opt, loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
