a02d9e05086284d9d69919732ef491dc2c61aeea
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-30 18:49:47 -0800

    Update __init__.py

--- hyper_resilient_experiments/__init__.py
+++ hyper_resilient_experiments/__init__.py
@@ -2,3 +2,5 @@ from .bi_tune import multi_train
 from .simple_mnist import pt_mnist, tf_mnist
 from .alexnet_cifar import pytorch_alexnet, tensorflow_alexnet
 from .segmentation import tensorflow_unet, pytorch_unet, gis_preprocess
+from .segmentation.UNet import pytorch_unet, tensorflow_unet
+
