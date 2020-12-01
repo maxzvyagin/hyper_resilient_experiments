06d313b9f4f7a2a54d5a60005968a324ba10b958
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-11-30 17:36:31 -0800

    imports are terrible

--- segmentation/tensorflow_unet.py
+++ segmentation/tensorflow_unet.py
@@ -1,4 +1,7 @@
 ### Tensorflow UNet with Resnet34 Backbone
+import os
+
+os.environ['SM_FRAMEWORK'] = 'tf.keras'
 import segmentation_models as sm
 import tensorflow as tf
 import tensorflow_datasets as tfds
@@ -7,6 +10,7 @@ import sys
 from tensorflow import keras
 
 from ..segmentation.gis_preprocess import tf_gis_test_train_split
+
 
 # from segmentation.UNet.tensorflow_unet import TensorFlow_UNet_Model
 # from model import unet
@@ -40,7 +44,7 @@ def cityscapes_tf_objective(config, classes=30):
     #                         data_dir='/home/mzvyagin/datasets/')
     # train = train.with_options(options).batch(b)
     # test = test.with_options(options).batch(b)
-    #res = model.fit(train, epochs=config['epochs'], batch_size=b)
+    # res = model.fit(train, epochs=config['epochs'], batch_size=b)
     res = model.fit(train, epochs=config['epochs'])
     res_test = model.evaluate(test)
     return res_test[1], model
@@ -48,16 +52,16 @@ def cityscapes_tf_objective(config, classes=30):
 
 # same model just using gis data instead
 def gis_tf_objective(config, classes=1):
-    #os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
     tf.random.set_seed(0)
     keras.backend.set_image_data_format('channels_last')
     b = int(config['batch_size'])
     strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1", "/gpu:2", "/gpu:3", "/gpu:4", "/gpu:5",
                                                        "/gpu:6", "/gpu:7"])
     with strategy.scope():
-    # model = keras.models.Sequential()
-    # model.add(make_tensorflow_unet(4, 1))
-    # model.add(keras.layers.Dense(1, activation="sigmoid"))
+        # model = keras.models.Sequential()
+        # model.add(make_tensorflow_unet(4, 1))
+        # model.add(keras.layers.Dense(1, activation="sigmoid"))
         model = sm.Unet('resnet34', activation="sigmoid")
         opt = tf.keras.optimizers.Adam(learning_rate=config['learning_rate'])
         model.compile(optimizer=opt, loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
@@ -86,25 +90,25 @@ def get_cityscapes():
     train = list(train)
     train_x = [pair['image_left'] for pair in train]
     train_y = [pair['segmentation_label'] for pair in train]
-    train_x = list(map(lambda x: x.numpy()/255.0, train_x))
+    train_x = list(map(lambda x: x.numpy() / 255.0, train_x))
     # train_x, train_y = [], []
     # for i in train:
     #     train_x.append(i['image_left'].numpy() / 255)
     #     train_y.append(i['segmentation_label'].numpy() / 255)
-    #test_x, test_y = [], []
+    # test_x, test_y = [], []
     test = list(test)
     test_x = [pair['image_left'] for pair in test]
     test_y = [pair['segmentation_label'] for pair in test]
-    train_x = list(map(lambda x: tf.convert_to_tensor(x.numpy()/255.0), test_x))
+    train_x = list(map(lambda x: tf.convert_to_tensor(x.numpy() / 255.0), test_x))
     # for i in test:
     #     test_x.append(i['image_left'].numpy() / 255)
     #     test_y.append(i['segmentation_label'].numpy() / 255)
     return (train_x, train_y), (test_x, test_y)
-    #return train, test
+    # return train, test
 
 
 if __name__ == "__main__":
     test_config = {'batch_size': 1, 'learning_rate': .001, 'epochs': 1}
-    #res = cityscapes_tf_objective(test_config)
+    # res = cityscapes_tf_objective(test_config)
     # print(res[0])
     res = gis_tf_objective(test_config)
