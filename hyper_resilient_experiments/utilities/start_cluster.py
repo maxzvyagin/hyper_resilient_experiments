c600b4c18b6b5a3195db6904554cca58da2a859c
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2020-12-11 11:19:21 -0800

    Update start_cluster.py

--- hyper_resilient_experiments/utilities/start_cluster.py
+++ hyper_resilient_experiments/utilities/start_cluster.py
@@ -10,6 +10,7 @@ def start_cluster(yaml="/home/mzvyagin/default_cluster.yaml", cluster_name="defa
     nodefile = os.environ['COBALT_NODEFILE']
     f = open(nodefile, "r")
     nodes = f.readlines()
+    f.close()
     num_workers = len(nodes)
     worker_nodes = []
     for x in nodes:
@@ -17,21 +18,24 @@ def start_cluster(yaml="/home/mzvyagin/default_cluster.yaml", cluster_name="defa
             nodes.remove(x)
         else:
             worker_nodes.append(x.strip())
+    print(worker_nodes)
     workers = str(worker_nodes)
     lines = f'''
-        cluster_name: default
-        provider:
-            type: local
-            head_ip: {host}
-            worker_ips: {workers}
-        min_workers: {num_workers}
-        max_workers: {num_workers}
-        setup_commands:
-            - source ~/.bashrc; conda activate dl
+cluster_name: default
+provider:
+    type: local
+    head_ip: {host}
+    worker_ips: {workers}
+min_workers: {num_workers}
+max_workers: {num_workers}
+setup_commands:
+    - source ~/.bashrc; conda activate dl
         '''
     f = open(yaml, "w+")
     f.write(lines)
-    os.popen('ray up '+yaml)
+    f.close()
+    print("Created yaml. Run the command: \n \tray up "+yaml)
+    # os.popen('ray up '+yaml)
 
 
 if __name__ == "__main__":
