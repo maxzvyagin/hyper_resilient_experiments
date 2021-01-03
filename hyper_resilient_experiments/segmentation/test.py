47d3b9f3527907053ccf2b5906fa00a19e7bc7de
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-03 14:36:26 -0800

    Update test.py

--- hyper_resilient_experiments/segmentation/test.py
+++ hyper_resilient_experiments/segmentation/test.py
@@ -6,4 +6,4 @@ files = [("/scratch/mzvyagin/Ephemeral_Channels/Imagery/vhr_2012_refl.img",
                  ("/scratch/mzvyagin/Ephemeral_Channels/Imagery/vhr_2014_refl.img",
                   "/scratch/mzvyagin/Ephemeral_Channels/Reference/reference_2014_merge.shp")]
 (x_train, y_train), (x_test, y_test) = tf_gis_test_train_split(img_and_shps=files)
-train = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(4, drop_remainder=True)
+train = tf.data.Dataset.from_tensor_slices((x_train, y_train))
