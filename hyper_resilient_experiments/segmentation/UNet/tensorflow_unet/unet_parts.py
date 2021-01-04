a06bed838ec359b59d8aea6193fe44ddef2243cc
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-04 10:45:14 -0800

    tf unet updates

--- hyper_resilient_experiments/segmentation/UNet/tensorflow_unet/unet_parts.py
+++ hyper_resilient_experiments/segmentation/UNet/tensorflow_unet/unet_parts.py
@@ -60,16 +60,16 @@ class Up(keras.layers.Layer):
     def call(self, x1, x2):
         x1 = self.up(x1)
 
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x = tf.concat([x2, x1], axis=1)
+        # # input is CHW
+        # diffY = x2.shape[2] - x1.shape[2]
+        # diffX = x2.shape[3] - x1.shape[3]
+        #
+        # x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+        #                 diffY // 2, diffY - diffY // 2])
+        # # if you have padding issues, see
+        # # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = tf.concat([x2, x1], axis=3)
         return self.conv(x)
 
 
--- hyper_resilient_experiments/segmentation/test.py
+++ hyper_resilient_experiments/segmentation/test.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from hyper_resilient_experiments.segmentation.gis_preprocess import tf_gis_test_train_split
 from hyper_resilient_experiments.segmentation.UNet import TensorFlow_UNet_Model
+import segmentation_models as sm
 
 config = {'batch_size': 1, 'learning_rate': .001, 'epochs': 1, 'adam_epsilon': 10**-9}
 
@@ -8,15 +9,16 @@ files = [("/scratch/mzvyagin/Ephemeral_Channels/Imagery/vhr_2012_refl.img",
                   "/scratch/mzvyagin/Ephemeral_Channels/Reference/reference_2012_merge.shp"),
                  ("/scratch/mzvyagin/Ephemeral_Channels/Imagery/vhr_2014_refl.img",
                   "/scratch/mzvyagin/Ephemeral_Channels/Reference/reference_2014_merge.shp")]
-#(x_train, y_train), (x_test, y_test) = tf_gis_test_train_split(img_and_shps=files)
-train = tf.data.experimental.load('tf_gis_dataset')
+(x_train, y_train), (x_test, y_test) = tf_gis_test_train_split(img_and_shps=files)
+train = tf.data.experimental.load('tf_gis_dataset', tf.TensorSpec((4, 256, 256, 1), dtype=tf.dtypes.float32))
 # creating a tf Dataset with this batch size takes _forever_
 # train = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(4)
 # model = tf.keras.Sequential()
 # model.add(make_tensorflow_unet(4, 1))
 # model.add(tf.keras.layers.Dense(1, activation=tf.nn.log_softmax))
-model = TensorFlow_UNet_Model(4, 1)
+# model = TensorFlow_UNet_Model(4, 1)
+model = sm.Unet('resnet34', input_shape=(256, 256, 4), encoder_weights=None)
 opt = tf.keras.optimizers.Adam(learning_rate=config['learning_rate'], epsilon=config['adam_epsilon'])
 model.compile(optimizer=opt, loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
-              metrics=['accuracy'], run_eagerly=True)
-model.fit(x_train, y_train, epochs=config['epochs'], batch_size=4)
+              metrics=['accuracy'])
+model.fit(train, epochs=config['epochs'])
