59d3edefc602ab068cee140a49a8110ea0f5f668
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-04 17:42:33 -0800

    Update tensorflow_alexnet.py

--- hyper_resilient_experiments/alexnet_cifar/tensorflow_alexnet.py
+++ hyper_resilient_experiments/alexnet_cifar/tensorflow_alexnet.py
@@ -52,7 +52,7 @@ class TensorFlow_AlexNet:
         opt = tf.keras.optimizers.Adam(learning_rate=config['learning_rate'], epsilon=config['adam_epsilon'])
         self.model.compile(optimizer=opt,
                            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                           metrics=['accuracy'])
+                           metrics=['accuracy'], run_eagerly=True)
         self.config = config
 
     def fit(self):
