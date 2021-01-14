c1fe4d71e829ccdd65a39eee0fa58748b4e0e5c1
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-14 14:39:42 -0800

    Create test_tensorflow.py

---
+++ hyper_resilient_experiments/verification/test_tensorflow.py
@@ -0,0 +1,18 @@
+from hyper_resilient_experiments.alexnet_cifar import pytorch_alexnet, tensorflow_alexnet
+import spaceray
+from ray import tune
+
+class Namespace:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+def objective(config):
+    acc, model = tensorflow_alexnet.cifar100_tf_objective(config)
+    search_results = {'average_res': acc}
+    tune.report(**search_results)
+    return search_results
+
+if __name__ == "__main__":
+    args = Namespace(json='../standard.json', trials=10, out='tensorflow_benchmark.csv')
+    spaceray.run_experiment(args, objective, ray_dir="/lus/theta-fs0/projects/CVD-Mol-AI/mzvyagin/raylogs", cpu=8,
+                                start_space=0, mode="max")
