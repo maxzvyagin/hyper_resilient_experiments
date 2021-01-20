7138a51a5860210a06ce0fcee1ef2200b99bd38e
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-01-19 19:14:11 -0800

    caltech

--- hyper_resilient_experiments/alexnet_caltech/caltech_tensorflow_alexnet.py
+++ hyper_resilient_experiments/alexnet_caltech/caltech_tensorflow_alexnet.py
@@ -64,7 +64,7 @@ def fashion_tf_objective(config):
     return accuracy, model.model
 
 def standardize(i):
-    return (tf.image.resize(i, (224, 224))).numpy()
+    return tf.image.resize_with_crop_or_pad(i, 300, 200).numpy()
 
 def get_caltech():
     """ Returns test, train split of Caltech data"""
